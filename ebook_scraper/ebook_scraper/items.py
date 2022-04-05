9deeadd6505fa2d8f86e33bac723c29aa10ad4da
Diego Barbo <diegobarbox@gmail.com>
2022-04-05 15:18:36 -0300

    using items in spiders

--- ebook_scraper/ebook_scraper/items.py
+++ ebook_scraper/ebook_scraper/items.py
@@ -1,12 +1,8 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy import Item, Field
 
 
-class EbookScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class EbookItem(scrapy.Item):
+    title = Field()
+    price = Field()
+    
--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -1,10 +1,18 @@
 import scrapy
+from ebook_scraper.items import EbookItem
+
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
     start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
-        print("[ parse ]")
-        
-        print(response.xpath("//h3/a/@title").get())
+        ebooks = response.css("article.product_pod")
+
+        for ebook in ebooks:
+            ebook_item = EbookItem()
+            
+            ebook_item['title'] = ebook.css("h3 a").attrib['title']
+            ebook_item['price'] = ebook.css("p.price_color::text").get()
+
+            yield ebook_item
