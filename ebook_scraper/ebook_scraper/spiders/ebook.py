64c96446e992ac4ab5a19f7e534f6332d8989c1d
Diego Barbo <diegobarbox@gmail.com>
2022-04-05 10:58:12 -0300

    selecting all the data

--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -7,4 +7,10 @@ class EbookSpider(scrapy.Spider):
     def parse(self, response):
         print("[ parse ]")
         
-        print(response.css("h3 a::text").get())
+        ebooks = response.css("article")
+        
+        for ebook in ebooks:
+            title = ebook.css('a::text').get()
+            price = ebook.css('p.price_color::text').get()
+
+            print(title, price)
