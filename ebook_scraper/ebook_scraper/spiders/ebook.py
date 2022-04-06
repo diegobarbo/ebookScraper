292d269e93c0c65d850a72490b269f9f8a0369b2
Diego Barbo <diegobarbox@gmail.com>
2022-04-06 17:12:14 -0300

    start_request() method

--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -7,14 +7,24 @@ from scrapy.loader import ItemLoader
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
-    start_urls = ["https://books.toscrape.com/catalogue/category/books/mystery_3"]
+    # start_urls = ["https://books.toscrape.com/catalogue/category/books/sequencial-art"]
 
     def __init__(self):
         super().__init__()
-        self.page_count = 0
+        self.page_count = 1
+        self.total_pages = 4
+
+    def start_requests(self):
+        base_url = "https://books.toscrape.com/catalogue/category/books/sequencial-art"
+
+        while self.page_count <= self.total_pages:
+            yield scrapy.Request(
+                f"{base_url}/page-{self.page_count}.html"
+            )
+            self.page_count += 1
 
     def parse(self, response):
-        self.page_count += 1
+        # self.page_count += 1
 
         ebooks = response.css("article.product_pod")
 
@@ -26,12 +36,12 @@ class EbookSpider(scrapy.Spider):
             
             yield loader.load_item()
 
-        print("[ PAGE COUNT ]:", self.page_count)
+        # print("[ PAGE COUNT ]:", self.page_count)
         
-        next_btn = response.css("li.next a")
+        # next_btn = response.css("li.next a")
 
-        if next_btn:
-            next_page = f"{self.start_urls[0]}/{next_btn.attrib['href']}"            
-            yield scrapy.Request(url=next_page)
+        # if next_btn:
+        #     next_page = f"{self.start_urls[0]}/{next_btn.attrib['href']}"            
+        #     yield scrapy.Request(url=next_page)
 
         
