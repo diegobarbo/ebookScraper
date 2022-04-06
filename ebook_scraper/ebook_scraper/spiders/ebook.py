1b5d5e6af41ff7db47513f6b5576623c6c72a775
Diego Barbo <diegobarbox@gmail.com>
2022-04-06 11:10:34 -0300

    extracting links from href attributes

--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -7,7 +7,7 @@ from scrapy.loader import ItemLoader
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
-    start_urls = ["https://books.toscrape.com/catalogue/category/books/travel_2/"]
+    start_urls = ["https://books.toscrape.com/catalogue/category/books/mystery_3/"]
     cols = ["Title", "Price"]
 
     def parse(self, response):
@@ -20,3 +20,9 @@ class EbookSpider(scrapy.Spider):
             loader.add_css('price', "p.price_color::text")
             
             yield loader.load_item()
+        
+        next_btn = response.css("li.next a")
+
+        print("[ NEXT PAGE URL ]:", 
+        f"{self.start_urls[0]}/{next_btn.attrib['href']}",
+        self.start_urls[0] + next_btn.attrib["href"])
