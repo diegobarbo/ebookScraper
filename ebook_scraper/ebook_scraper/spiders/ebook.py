b1062c5b2f03a16a95ed17119853bf720e346941
Diego Barbo <diegobarbox@gmail.com>
2022-04-06 11:22:22 -0300

    send request to the next page

--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -7,10 +7,15 @@ from scrapy.loader import ItemLoader
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
-    start_urls = ["https://books.toscrape.com/catalogue/category/books/mystery_3/"]
-    cols = ["Title", "Price"]
+    start_urls = ["https://books.toscrape.com/catalogue/category/books/mystery_3"]
+
+    def __init__(self):
+        super().__init__()
+        self.page_count = 0
 
     def parse(self, response):
+        self.page_count += 1
+
         ebooks = response.css("article.product_pod")
 
         for ebook in ebooks:
@@ -20,9 +25,13 @@ class EbookSpider(scrapy.Spider):
             loader.add_css('price', "p.price_color::text")
             
             yield loader.load_item()
+
+        print("[ PAGE COUNT ]:", self.page_count)
         
         next_btn = response.css("li.next a")
 
-        print("[ NEXT PAGE URL ]:", 
-        f"{self.start_urls[0]}/{next_btn.attrib['href']}",
-        self.start_urls[0] + next_btn.attrib["href"])
+        if next_btn:
+            next_page = f"{self.start_urls[0]}/{next_btn.attrib['href']}"            
+            yield scrapy.Request(url=next_page)
+
+        
