bc96f2c922397c24b3cad9374dd7195f9df0a2ab
Diego Barbo <diegobarbox@gmail.com>
2022-04-05 22:35:07 -0300

    saving data to mongodb

--- ebook_scraper/ebook_scraper/pipelines.py
+++ ebook_scraper/ebook_scraper/pipelines.py
@@ -1,18 +1,21 @@
-from openpyxl import Workbook
+from pymongo import MongoClient
 from itemadapter import ItemAdapter
 
 
 class EbookScraperPipeline:
 
     def open_spider(self, spider):
-        self.workbook = Workbook()
-        self.sheet = self.workbook.active
-        self.sheet.title = "ebooks"
-
-        self.sheet.append(spider.cols)
+        self.client = MongoClient(
+            host="mongodb+srv://diego:kolip159@ebookscraper.fo7pf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+            connect=False
+        )
+        self.collection = self.client.get_database("ebook").get_collection("travel")
 
     def process_item(self, item, spider):
-        self.sheet.append([ item['title'], item['price'] ])
+        self.collection.insert_one(
+            ItemAdapter(item).asdict()
+        )
+        return item
 
     def close_spider(self, spider):
-        self.workbook.save("ebooks.xlsx")
+        self.client.close()
